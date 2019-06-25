00708fb39d8b00d269162430b579409280873b1f
timcrawf <timcrawf88@gmail.com>
2019-06-25 10:20:29 -0500

    removed repeat

--- log_parser.py
+++ log_parser.py
@@ -6,7 +6,7 @@ parser.add_option("-l", "--log", dest='log_location',
                   help="Path to log file")
 parser.add_option("-b", "--browser", dest='rank',
                   help="Most popular browser at rank")
-parser.add_option("-m", "--malicious", action="store_const", const= 'Python,curl,Spider,Nimbostratus-Bot,Nimbostratus-Bot,YandexBot',
+parser.add_option("-m", "--malicious", action="store_const", const= 'Python,curl,Spider,Nimbostratus-Bot,YandexBot',
                   dest='search_terms', help="Print any potentionaly malicious traffic, based on\nsearch of Pyhton, curl, Spider, Nimbostratus-Bot, and YandexBot")
 parser.add_option("-s", "--search-terms", dest='search_terms',
                   help="search browser data by comma separated list")
